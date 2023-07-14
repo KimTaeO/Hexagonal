1255cc9cd32aaae022f1232ff4600c0d93a0a5e3
KimTaeO <taeo0328@gmail.com>
2023-07-14 14:45:30 +0900

    :sparkles: :: 순수한 도메인으로 변경

--- src/main/kotlin/com/example/hexagonal/board/domain/Board.kt
+++ src/main/kotlin/com/example/hexagonal/board/domain/Board.kt
@@ -1,16 +1,7 @@
 package com.example.hexagonal.board.domain
 
-import com.example.hexagonal.board.adapter.output.BoardJpaEntity
-import javax.persistence.*
-
 data class Board (
-    val id: Long = 0L,
+    val id: Long,
     val title: String,
     val content: String
-) {
-    fun toEntity(): BoardJpaEntity =
-        BoardJpaEntity(
-            title = this.title,
-            content = this.content
-        )
-}
+)
