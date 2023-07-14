8ca51be976e9e2c3f82dc38c74a9d4b86be3b23e
KimTaeO <taeo0328@gmail.com>
2023-07-15 00:41:32 +0900

    :sparkles: :: Update Board Controller 작성

--- src/main/kotlin/com/example/hexagonal/board/adapter/input/web/BoardController.kt
+++ src/main/kotlin/com/example/hexagonal/board/adapter/input/web/BoardController.kt
@@ -1,23 +1,21 @@
 package com.example.hexagonal.board.adapter.input.web
 
 import com.example.hexagonal.board.adapter.input.web.request.CreateBoardRequest
+import com.example.hexagonal.board.adapter.input.web.request.UpdateBoardRequest
 import com.example.hexagonal.board.adapter.input.web.response.GetBoardResponse
 import com.example.hexagonal.board.application.port.input.CreateBoardUseCase
 import com.example.hexagonal.board.application.port.input.GetBoardUseCase
+import com.example.hexagonal.board.application.port.input.UpdateBoardUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/board")
 class BoardController(
     private val createBoardUseCase: CreateBoardUseCase,
-    private val getBoardUseCase: GetBoardUseCase
+    private val getBoardUseCase: GetBoardUseCase,
+    private val updateBoardUseCase: UpdateBoardUseCase
 ) {
     @PostMapping
     fun createBoard(@RequestBody createBoardRequest: CreateBoardRequest): ResponseEntity<Void> {
@@ -29,5 +27,11 @@ class BoardController(
     fun getBoard(@PathVariable("board_id") boardId: Long): ResponseEntity<GetBoardResponse> {
         val result = getBoardUseCase.getBoard(boardId)
         return ResponseEntity.ok().body(result)
+    }
+
+    @PatchMapping("/{board_id}")
+    fun updateBoard(@PathVariable("board_id") boardId: Long, @RequestBody updateBoardRequest: UpdateBoardRequest): ResponseEntity<Void> {
+        updateBoardUseCase.updateBoard(boardId, updateBoardRequest)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 }
