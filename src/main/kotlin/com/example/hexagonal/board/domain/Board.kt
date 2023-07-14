e35be5ba5b97364decabd3fabfede811b20757c0
KimTaeO <taeo0328@gmail.com>
2023-07-15 00:49:08 +0900

    :sparkles: :: 도메인 변환 함수 작성

--- src/main/kotlin/com/example/hexagonal/board/domain/Board.kt
+++ src/main/kotlin/com/example/hexagonal/board/domain/Board.kt
@@ -1,7 +1,16 @@
 package com.example.hexagonal.board.domain
 
+import com.example.hexagonal.board.adapter.output.BoardJpaEntity
+
 data class Board (
-    val id: Long,
+    val id: Long = 0L,
     val title: String,
     val content: String
-)
+) {
+    fun toEntity(): BoardJpaEntity =
+        BoardJpaEntity(
+            id = this.id,
+            title = this.title,
+            content = this.content
+        )
+}
