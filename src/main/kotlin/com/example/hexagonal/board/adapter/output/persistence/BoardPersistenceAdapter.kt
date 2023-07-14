ff4a71c757b796ad1ada1443a261c7b5f3d8497e
KimTaeO <taeo0328@gmail.com>
2023-07-15 00:59:46 +0900

    :sparkles: :: Delete Board 아웃바운트 Port 작성

--- src/main/kotlin/com/example/hexagonal/board/adapter/output/persistence/BoardPersistenceAdapter.kt
+++ src/main/kotlin/com/example/hexagonal/board/adapter/output/persistence/BoardPersistenceAdapter.kt
@@ -3,6 +3,7 @@ package com.example.hexagonal.board.adapter.output.persistence
 import com.example.hexagonal.board.adapter.output.BoardJpaEntity
 import com.example.hexagonal.board.adapter.output.persistence.repository.BoardRepository
 import com.example.hexagonal.board.application.port.output.CreateBoardPort
+import com.example.hexagonal.board.application.port.output.DeleteBoardPort
 import com.example.hexagonal.board.application.port.output.GetBoardPort
 import com.example.hexagonal.board.domain.Board
 import org.springframework.data.repository.findByIdOrNull
@@ -11,11 +12,15 @@ import org.springframework.stereotype.Service
 @Service
 class BoardPersistenceAdapter(
     private val boardRepository: BoardRepository
-): CreateBoardPort, GetBoardPort {
+): CreateBoardPort, GetBoardPort, DeleteBoardPort {
     override fun saveBoard(board: Board) {
         boardRepository.save(board.toEntity())
     }
 
     override fun getBoard(id: Long): BoardJpaEntity? =
         boardRepository.findByIdOrNull(id)
+
+    override fun deleteBoard(id: Long) {
+        boardRepository.deleteById(id)
+    }
 }
