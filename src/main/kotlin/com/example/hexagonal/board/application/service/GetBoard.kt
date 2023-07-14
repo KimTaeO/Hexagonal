c4021fb5033be66d57281bd53ce7717cd2f43255
KimTaeO <taeo0328@gmail.com>
2023-07-14 14:39:51 +0900

    :sparkles: GetBoard 서비스 작성

---
+++ src/main/kotlin/com/example/hexagonal/board/application/service/GetBoard.kt
@@ -0,0 +1,22 @@
+package com.example.hexagonal.board.application.service
+
+import com.example.hexagonal.board.adapter.input.web.response.GetBoardResponse
+import com.example.hexagonal.board.application.port.input.GetBoardUseCase
+import com.example.hexagonal.board.application.port.output.GetBoardPort
+import com.example.hexagonal.board.domain.Board
+import org.springframework.stereotype.Service
+
+@Service
+class GetBoard(
+    private val getBoardPort: GetBoardPort
+): GetBoardUseCase {
+    override fun getBoard(id: Long): GetBoardResponse {
+        val board = getBoardPort.getBoard(id)
+        return GetBoardResponse(
+            id = board.id,
+            title = board.title,
+            content = board.content
+        )
+    }
+
+}
